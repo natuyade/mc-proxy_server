29783a36cd3f200001698953a05ddadeb133dea7
natuyade <natuyade@tuta.io>
2026-03-17 23:38:38 +0900

    learning first bit and value bit

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> Result<(), std::io::Error> {
                 let mut buffer = [0u8; 1024];
 
                 // read(&mut buffer)で受信したbyte列を可変bufferに書き込む.
-                // read()は一回(/= 1packet)で受け取れるbyteの数は流れによって変わる
+                // read()は一回(!= 1packet)で受け取れるbyteの数は流れによって変わる
                 // 一回で1byteきたり... 一回で100bytesきたり...
                 match socket.read(&mut buffer).await {
 
@@ -40,7 +40,7 @@ async fn main() -> Result<(), std::io::Error> {
                         println!("client disconnected {addr}");
                     }
 
-                    // 受信したbyteデータを表示, 16進数へ変換,表示(
+                    // 受信したbyteデータを表示, 先頭に1byte存在していれば16進数へ変換,表示(
                     // read(1) => 10進数=49 -> 16進数=31,
                     // read(7) => 10進数=55 -> 16進数=37
                     // ) (あくまでも流れの説明)
@@ -48,40 +48,48 @@ async fn main() -> Result<(), std::io::Error> {
                         println!("read {n}bytes data");
                         println!("{:?}", &buffer[0..n]);
 
-                        // hex = 16進数
-                        let hex = buffer[..n]
-                            .iter()
-                            // mapでiterで取り出したものを文字列へ変換
-                            // {:02X}の意味
-                            // X=数値を16進数に大文字変換(x=小文字)
-                            // 2=表示最小桁数
-                            // 0=桁が最小桁数に満たなければ0で埋める
-                            // これでみんながよくみる`あれ`になる.
-                            // format!の場合変数が参照されたものでも扱えるが
-                            // ここでは型意識のために*で参照を外し実体を扱う
-                            .map(|b| format!("{:02X}", *b))
-                            // mapでformatしたものをそれぞれcollect()のVecの中に集める(collect)
-                            .collect::<Vec<_>>()
-                            // それぞれの要素の間に" "(空白)を入れ連結
-                            .join(" ");
+                        match buffer[..n].get(0) {
+                            Some(first) => {
+                                println!("first byte: {}", *first);
+                                println!("first hex: {:02X}", *first);
 
-                        println!("{hex}");
+                                // 0b=後に続くものが二進数であることを表す
+                                // 1000_0000=二進数での128.この状態を最上位ビットが立っている状態ともいう.
 
-                        // buffer[0..3]とすると,受け取ったbyte列が二つの場合,
-                        // 2番目(3個目)は今回受信したデータ以降の場所を見てしまう.
-                        // そのため一度すべて受け取ってから読む範囲を指定する
-                        // minで指定すれば3まではそのまま表示,それ以降は端折られる
-                        let head_len = n.min(3);
+                                // VarInt等の世界で8bitの最上位bitは,主にバイトの続きがあるかを表し
+                                // 1で継続,0が最終バイト.を意味する
+                                // 以降7bitは実データを表す
 
-                        let head_hex = buffer[..head_len]
-                            .iter()
-                            .map(|b| format!("{:02X}", *b))
-                            .collect::<Vec<_>>()
-                            .join(" ");
+                                // 例えばfirstが1101_0011だとして&(bit AND演算)で
+                                // 最上位bitだけが立ったマスク(1000_0000)と比較し,
+                                // 1101_0011
+                                // 1000_0000
+                                // ---------
+                                // 1000_0000になる
 
-                        println!("convert {head_len}bytes");
+                                // 分かりやすくすると
+                                // (0 & 0 = 0), (1 & 0 = 0), (1 & 1 = 1)
 
-                        println!("{head_hex}");
+                                // そうすればfirstの最上位bitが立っていることが判明し
+                                // 1000_0000(128) != 0 でtrueになる.
+                                // 最上位bitが立っていたら(0でなければ)次のバイトが存在し継続するというboolになる
+                                let has_next = (*first & 0b1000_0000) != 0;
+                                println!("has next byte: {has_next}");
+
+                                // こっちは逆に最上位以外の下位7bitを取り出し.
+                                // 1101_0011
+                                // 0111_1111
+                                // ---------
+                                // 0101_0011になる
+                                // firstから値部分を取り出す
+                                let value_part = *first & 0b0111_1111;
+                                println!("value part: {value_part}");
+                                println!("value part hex: {:02X}",value_part);
+                            }
+                            None => {
+                                println!("no first byte")
+                            }
+                        }
                     }
 
                     Err(e) => {
