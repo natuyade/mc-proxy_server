26f7ad43b86667455b9ca9eb64f003c00c5652f8
natuyade <natuyade@tuta.io>
2026-03-27 22:35:02 +0900

    fix dir path revenge

--- src/rewrite_path.rs
+++ src/rewrite_path.rs
@@ -7,32 +7,23 @@ pub fn rewrite_path(file_path_into: impl Into<PathBuf>, file_name: impl Into<Str
 
     // stemはファイルの名前全体の幹の部分(拡張子を除いた名前の部分)
     // extensionはその名の通り, 拡張子のことを表す
-    let file_path = file_path_into.into();
-    let new_name = file_name.into();
-    let new_extension = file_extension.into();
+    let mut file_path = file_path_into.into();
+    let file_stem = file_name.into();
+    let file_extension = file_extension.into();
 
-    let mut stem = match file_path.file_stem() {
-        Some(stem) => stem.to_os_string(),
-        None => OsString::new(),
-    };
-    let mut extension = match file_path.extension() {
-        Some(extension) => extension.to_os_string(),
-        None => OsString::new(),
-    };
+    let mut stem = OsString::new();
+    let mut extension = OsString::new();
 
-    // clearで中身を空のOsStringに
-    stem.clear();
-    // pushで文字を追加
-    stem.push(new_name);
+    stem.push(file_stem);
+    extension.push(file_extension);
 
-    // 拡張子
-    extension.clear();
-    extension.push(new_extension);
+    let mut dir_path = file_path.clone();
 
-    let mut new_file_path = file_path.clone();
-    let dir_path = file_path.clone();
-    new_file_path.set_file_name(stem);
-    new_file_path.set_extension(extension);
+    dir_path.set_file_name("");
+    dir_path.set_extension("");
 
-    (new_file_path, dir_path)
+    file_path.set_file_name(stem);
+    file_path.set_extension(extension);
+
+    (file_path, dir_path)
 }
